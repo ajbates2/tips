dec9cbf544bc1d8b4032e2ed250f2633c463d36f
AJBates2 <aj.bates2.92@gmail.com>
2020-08-26 18:58:37 -0500

    updated config

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
-    API_ENDPOINT: 'https://dry-caverns-05956.herokuapp.com/api',
-    TOKEN_KEY: 'make-that-shmoney',
+    API_ENDPOINT: process.env.REACT_APP_API_BASE_URL ||
+        "https://dry-caverns-05956.herokuapp.com/api",
+    TOKEN_KEY: process.env.TOKEN_KEY,
 }
