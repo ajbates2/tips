201dec583edb970b952e2e9c893d1c9428805d85
AJBates2 <aj.bates2.92@gmail.com>
2020-08-30 21:56:41 -0500

    updated endpoint

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
     API_ENDPOINT: process.env.REACT_APP_API_BASE_URL ||
-        "https://tips-sigma.vercel.app/api",
+        "https://dry-caverns-05956.herokuapp.com/api",
     TOKEN_KEY: process.env.TOKEN_KEY,
 }
