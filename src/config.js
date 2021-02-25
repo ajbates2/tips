61b81b1c996e06626c657713d13ed3336daee86b
AJ Bates <aj.bates2.92@gmail.com>
2021-02-24 18:47:47 -0600

    troubleshooting

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
-    API_ENDPOINT: process.env.REACT_APP_API_BASE_URL,
-       //"https://dry-caverns-05956.herokuapp.com/api",
+    API_ENDPOINT: process.env.REACT_APP_API_BASE_URL ||
+       "https://dry-caverns-05956.herokuapp.com/api",
     // API_ENDPOINT: "http://localhost:8000/api",
     TOKEN_KEY: process.env.TOKEN_KEY,
 }
