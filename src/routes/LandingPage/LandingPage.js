d851c44d76a3b2fa641dea71f482f66fc5a416d2
AJ Bates <aj.bates2.92@gmail.com>
2021-04-14 20:32:22 -0500

    updated landing page

--- src/routes/LandingPage/LandingPage.js
+++ src/routes/LandingPage/LandingPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './LandingPage.css';
 import TokenService from '../../services/token-service';
+import { Link } from 'react-router-dom';
 
 export default class LandingPage extends Component {
 	componentDidMount() {
@@ -12,13 +13,30 @@ export default class LandingPage extends Component {
 	render() {
 		return (
 			<div className='landing'>
+				<nav className='landing_nav'>
+					<ul className='nav-list'>
+						<li>
+							<Link to={`/login`}>login</Link>
+						</li>
+						<li>
+							<Link to={`/register`}>sign up</Link>
+						</li>
+					</ul>
+				</nav>
 				<header className='landing_header cartog_background'>
 					<div>
 						<h1>tips.</h1>
 						<p>Keep track of your money.</p>
 					</div>
 				</header>
-				<main className='landing_main'>
+				<div className='demo_creds'>
+					<h3>Demo Credentials</h3>
+					<p>
+						email: foo@bar.com <br />
+						password: password
+					</p>
+				</div>
+				<section className='landing_main'>
 					<div className='landing_main_container'>
 						<h2>Under Construction</h2>
 						<p>
@@ -49,7 +67,7 @@ export default class LandingPage extends Component {
 							</button>
 						</div>
 					</div>
-				</main>
+				</section>
 			</div>
 		);
 	}
